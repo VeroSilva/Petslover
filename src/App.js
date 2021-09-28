1441c50bd9d5d99135524f1ec37a46da5476370d
Veronica Silva <veronicasilvabarreto@gmail.com>
2021-09-28 18:55:55 -0500

    Componente Logo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 import { GlobalStyle } from './styles/GlobalStyles'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </>
