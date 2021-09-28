1196581879b474a572831487466af72cf6f9342f
Veronica Silva <veronicasilvabarreto@gmail.com>
2021-09-28 17:50:51 -0500

    Componente ListOfCategories

---
+++ src/components/ListOfCategories/styles.js
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components'
+// import { slideDown } from '../../styles/animation'
+
+export const List = styled.ul`
+    display: flex;
+    overflow: scroll;
+    width: 100%;
+    ${props => props.fixed && css`
+      {
+        background-color: #ffffff;
+        border-radius: 60px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+        left: 0;
+        right: 0;
+        top: -20px;
+        margin: 0 auto;
+        max-width: 400px;
+        padding: 5px;
+        position: fixed;
+        transform: scale(.5);
+        z-index: 99999;
+      }
+    `}
+`
+export const Item = styled.li`
+    padding: 0 8px;
+`
