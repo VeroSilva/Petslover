4617788d0ad1c67a27c71e1ac7fec9704424b4d9
Veronica Silva <veronicasilvabarreto@gmail.com>
2021-09-28 23:49:34 -0500

    Adding Link instead of achor tag

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -4,7 +4,7 @@ import { Link, Image } from './styles'
 const DEFAULT_IMAGE = 'https://imgur.com/dJa0Hpl.jpg'
 
 export const Category = ({ cover = DEFAULT_IMAGE, path = '#', emoji = '?' }) => (
-  <Link href={path}>
+  <Link to={path}>
     <Image src={cover} />
     {emoji}
   </Link>
