e31956f8a45250db435f43af649620adb10ad7de
Veronica Silva <veronicasilvabarreto@gmail.com>
2021-09-28 19:38:19 -0500

    Componente ListOfCategories modified

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,16 +1,47 @@
-import React from 'react'
-import { categories } from '../../../api/db.json'
+import React, { useState, useEffect } from 'react'
 
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    // setLoading(true)
+    window.fetch('https://petgram-server-vero-verosilva.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        console.log(response)
+        setCategories(response)
+        // setLoading(false)
+      })
+  }, [])
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
-        categories.map(category => <Item key={category}><Category {...category} /></Item>)
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
