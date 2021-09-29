05caa202f572f759f1073ad8611445c018ab663e
Veronica Silva <veronicasilvabarreto@gmail.com>
2021-09-28 23:57:48 -0500

    Detail page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React from 'react'
 import { Router } from '@reach/router'
 
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+import Detail from './pages/Detail'
 import { Home } from './pages/Home'
 
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-
   return (
     <>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:id' />
-          </Router>
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
     </>
   )
 }
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -3,6 +3,7 @@ import { useNearScreen } from '../../hooks/useNearScreen'
 import { useToogleLikeMutation } from '../../hooks/useToogleLikeMutation'
 import { Article, ImgWrapper, Img } from './styles'
 import { FavButton } from '../FavButton'
+import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -23,11 +24,11 @@ export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
       {
         show &&
           <>
-            <a href={`/?detail=${id}`}>
+            <Link to={`/detail/${id}`}>
               <ImgWrapper>
                 <Img src={src} />
               </ImgWrapper>
-            </a>
+            </Link>
 
             <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
           </>
