331d709d14ca510429ae2dfaf3db90eea8bd0859
Veronica Silva <veronicasilvabarreto@gmail.com>
2021-09-30 11:16:27 -0500

    Cypress tests

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useGetPhotos } from '../../hooks/useGetPhotos'
 import { PhotoCard } from '../PhotoCard'
+import { PhotoLoader } from '../Loaders/PhotoLoader'
 
 export const ListOfPhotoCards = ({ categoryId }) => {
   const { loading, error, data } = useGetPhotos(categoryId)
@@ -9,7 +10,7 @@ export const ListOfPhotoCards = ({ categoryId }) => {
     return <h2>Internal Server Error</h2>
   }
   if (loading) {
-    return <h2>Loading...</h2>
+    return <PhotoLoader />
   }
 
   return (
